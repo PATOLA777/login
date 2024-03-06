1e80d9f82616351a3bd1f9d4680968e40f857de1
PATOLA777 <129869511+PATOLA777@users.noreply.github.com>
2024-03-06 10:56:42 -0300

    codigo corrigido

--- cadastro.php
+++ cadastro.php
@@ -1,18 +1,36 @@
 <?php
-
 $login = $_POST['login'];
 $senha = MD5($_POST['senha']);
-$connect = mysql_connect('nome_do_servidor','nome_de_usuario','senha');
-$db = mysql_select_db('nome_do_banco_de_dados');
+$connect = mysqli_connect('local host','root','');
+$db = mysqli_select_db($connect,'login');
 $query_select = "SELECT login FROM usuarios WHERE login = '$login'";
-$select = mysql_query($query_select,$connect);
-$array = mysql_fetch_array($select);
+$select = mysqli_query($connect,$query_select);
+$arrayb= mysqli_fetch_array($select);
 $logarray = $array['login'];
 
-if($login== || $login== null){
+  if($login == "" || $login ==null){
     echo"<script language='javascript' type='text/javascript'>
-    alert('O campo login deve ser preenchido');window.lcation.href='
-    cadastro.html';</scrip>";
+    alert('O campo login deve ser preenchido');window.location.href='cadastro.html';</script>";
+
+
+  }else{
+    if($logarry == $login){
+        echo"<script language='javascript' type='text/javascript'>
+        alert('Esse login já existe');window.location.href='cadastro.html';</script>";
+        die();
 
     }else{
-      if($logarray == $login){
+        $query = "INSERT INTO usuarios (login,senha) VALUES ('$login','$senha')";
+        $insert = mysqli_query($connect,$query);
+
+        if($insert){
+            echo"<script language='javascript' type='text/javascript'>
+            alert('Usuário cadastrado com sucesso!');window.location.href='login.html'</script>";
+        }else{
+            echo"<script language='javascript' type='text/javascript'>
+            alert('Nao foi possível cadastrar esse usuário');window.location
+            .href='cadastro.html'</script>";
+    }
+  }
+}
+?>
